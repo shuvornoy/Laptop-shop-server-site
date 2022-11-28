ff4c6cfb696682b599f0df37dca21441fd87d39b
shuvornoy <shuvornoy@gmaol.com>
2022-11-28 18:01:12 +0600

    all complet server site

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ async function run() {
 run().catch(console.log);
 
 app.get("/", async (req, res) => {
-  res.send("laptop shop is server running");
+  res.send("laptop shop is server running site");
 });
 
 app.listen(port, () => console.log(`laptop shop running on ${port}`));
